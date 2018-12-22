158c5b70c7e203fae9affeabb095988f4cba3bff
sgliput <sgliput@comcast.net>
2018-12-22 16:11:36 -0500

    Updates

---
+++ precache-manifest.eb24580229c1be5362ef7f80a64968cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "6ff6047d21177a1ae97e",
+    "url": "/react-gh-pages/static/js/main.6ff6047d.chunk.js"
+  },
+  {
+    "revision": "1c2279e5c6f2e1dd17ce",
+    "url": "/react-gh-pages/static/js/1.1c2279e5.chunk.js"
+  },
+  {
+    "revision": "6ff6047d21177a1ae97e",
+    "url": "/react-gh-pages/static/css/main.0517f573.chunk.css"
+  },
+  {
+    "revision": "06aace31233271026c15d05924a60d37",
+    "url": "/react-gh-pages/index.html"
+  }
+];
